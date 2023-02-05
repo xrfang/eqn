cd4cca179e0d2871e4cb2d3e980f093b94850cc4
xrfang <xrfang@gmail.com>
2023-02-05 20:00:46 +0800

    added error logging

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	ver := flag.Bool("version", false, "show version info")
 	flag.Usage = func() {
 		fmt.Printf("Equation Solver and Plotter %s\n", verinfo())
-		fmt.Printf("\nUSAGE: %s [OPTIONS] <equation-file.eqn>\n", filepath.Base(os.Args[0]))
+		fmt.Printf("\nUSAGE: %s [OPTIONS] <recipe.eqn>\n", filepath.Base(os.Args[0]))
 		fmt.Println("\nOPTIONS")
 		flag.PrintDefaults()
 	}
@@ -21,10 +21,23 @@ func main() {
 		os.Exit(0)
 	}
 	if len(flag.Args()) == 0 {
-		fmt.Println("ERROR: missing equation file (-help for usage)")
+		fmt.Println("ERROR: missing recipe (-help for usage)")
 		os.Exit(1)
 	}
-	r, err := LoadRecipe(flag.Arg(0))
+	recipe := flag.Arg(0)
+	lf, err := os.Create(recipe + ".log")
+	assert(err)
+	defer func() {
+		err := allege(recover())
+		if err != nil {
+			fmt.Fprintln(lf, err.Error())
+		}
+		assert(lf.Close())
+		if err == nil {
+			os.Remove(recipe + ".log")
+		}
+	}()
+	r, err := LoadRecipe(recipe)
 	assert(err)
 	assert(r.Calculate())
 	assert(r.Solve())
